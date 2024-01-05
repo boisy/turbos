ac367dbd6e77c4a99b91a408931709eb1a36e168
Boisy Pitre <boisy@pitre.org>
2024-01-05 07:57:26 -0600

    F256 additions

---
+++ ports/f256/README.md
@@ -0,0 +1,11 @@
+# TurbOS (Foenix F256 Port)
+
+## Building
+
+To build an image that you can load into the Foenix Retro Systems F256 with the FNX6809, type: `make clean upload`. This builds and uploads the
+loader into RAM using the [FoenixMgr tools](https://github.com/pweingar/FoenixMgr).
+
+## Running
+The `go` program is a simple test program that increments the first character of the screen.
+
+See [go.asm](go.asm) for more information.
---
+++ ports/f256/go2.asm
@@ -0,0 +1,60 @@
+*******************************************************************************
+* TurbOS
+*******************************************************************************
+* See LICENSE.txt for licensing information.
+********************************************************************
+*
+* $Id$
+*
+* Edt/Rev  YYYY/MM/DD  Modified by
+* Comment
+* ------------------------------------------------------------------
+*   1      2023/08/29  BGP
+* Created.
+
+               nam       go
+               ttl       Very simple initial program
+
+               use       defsfile
+
+tylg           set       Prgrm+Objct
+atrv           set       ReEnt+rev
+rev            set       $01
+edition        set       1
+
+               mod       eom,name,tylg,atrv,start,size
+
+               org       0
+stack          rmb       200
+size           equ       .
+
+name           fcs       /go2/
+               fcb       edition
+
+start          equ       *
+
+
+               pshs      cc
+               orcc      #IntMasks
+               lda       #$C2
+               sta       >MMU_SLOT_1
+               ldd       #'T*256+'u
+               std       $2000
+               ldd       #'r*256+'b
+               std       $2002
+               ldd       #'O*256+'S
+               std       $2004
+               puls      cc
+
+loop@          pshs      cc
+               orcc      #IntMasks
+               inc       $2000
+               puls      cc
+               ldx       #$10
+               os9       F$Sleep
+               bra       loop@
+
+               emod
+eom            equ       *
+               end
+
