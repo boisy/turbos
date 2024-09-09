168cea6652d42223e629cd627495e0d270a1e45c
Boisy Gene Pitre <bpitre@apple.com>
2024-09-09 07:21:28 -0500

    Fully commented fmem.asm Made a baseline image and one for VDG. This is the start of measuring code and data size.

--- source/kernel/fmem.asm
+++ source/kernel/fmem.asm
@@ -16,7 +16,7 @@
 ;;;
 ;;; Change the process' data area size.
 ;;;
-;;; Entry:  D = The size to expand the memory area to (0 = return the current size).
+;;; Entry:  D = The size to expand or contract the memory area to (0 = return the current size).
 ;;;
 ;;; Exit:   Y = The address of the new memory area's upper bound.
 ;;;         D = The size of the new memory area, in bytes.
@@ -26,49 +26,52 @@
 ;;;        CC = Carry flag set to indicate error.
 ;;;
 ;;; F$Mem expands or contracts the process’ data memory area to the specified size. If you specify zero as the new size,
-;;; the current size and upper boundaries of data memory is returned.
+;;; the current size and upper boundaries of data memory are returned.
+;;;
 ;;; F$Mem rounds the size up to the next page boundary. Allocating additional memory continues upward from the previous
 ;;; highest address. Deallocating unneeded memory continues downward from that address.
 
 FMem           ldx       <D.Proc             get the current process descriptor
                ldd       R$A,u               get the size of the requested memory area
-               beq       returnsize@         branch if 0
-               bsr       RoundUpD            round up requested memory area
-               subb      P$PagCnt,x          subtract the current page count from B
-               beq       returnsize@         branch if 0
-               bcs       L0207               branch if less than 0
-               tfr       d,y                 else transfer requested memory area to Y
+               beq       returnsize@         branch if 0; caller is requesting size
+               bsr       RoundUpD            round up requested memory area to the next page; clear A
+               subb      P$PagCnt,x          subtract the current page count from B; now holds amount to add/shrink
+               beq       returnsize@         branch if 0; nothing more to allocate
+               bcs       memshrink@          branch if less than 0; we're shrinking memory
+               tfr       d,y                 else we're adding memory; transfer the additional request to Y
                ldx       P$ADDR,x            get the process' base data address page and page count in X
                pshs      u,y,x               save off registers
-_stkPageAddr@  set       0
-_stkPageCnt@   set       1
-_stkReqMem@    set       2
+_stkPageAddr@  set       0                   bits 15-8 of X (P$ADDR)
+_stkPageCnt@   set       1                   bits 7-0 of X (P$PagCnt)
+_stkStrtPg@    set       2                   bits 15-8 of Y (initially $00; used as starting page of memory to allocate)
+_stkReqPg@     set       3                   bits 7-0 of Y (additional pages needed to fulfill request)
                ldb       _stkPageAddr@,s     get the page address from the stack
                beq       L01E1               branch if it's 0
-               addb      _stkPageCnt@,s      add it to te page count from the stack
+               addb      _stkPageCnt@,s      add it to the page count from the stack
 L01E1          ldx       <D.FMBM             get the address of the start of the free memory bitmap
                ldu       <D.FMBM+2           and the address of the end of the free memory bitmap
                os9       F$SchBit            search for the location
                bcs       ex@                 branch if there was an error
-               stb       _stkReqMem@,s       save it the location to the stack
-               ldb       _stkPageAddr@,s
-               beq       L01F6
-               addb      _stkPageCnt@,s
-               cmpb      _stkReqMem@,s
-               bne       ex@
-L01F6          ldb       _stkReqMem@,s
-               os9       F$AllBit            allocate the bits
-               ldd       _stkReqMem@,s
-               suba      _stkPageCnt@,s
-               addb      _stkPageCnt@,s
-               puls      u,y,x
+               stb       _stkStrtPg@,s       B = starting page of potentially allocated memory; save it to the stack
+               ldb       _stkPageAddr@,s     get the page address from the stack
+               beq       L01F6               branch if 0
+               addb      _stkPageCnt@,s      add it to the page count from the stack (points to last page of memory + 1)
+               cmpb      _stkStrtPg@,s       compare it to the starting page of the potentially allocated memory
+               bne       ex@                 should be the same; if not, we're out of memory so branch
+L01F6          ldb       _stkStrtPg@,s       get the starting page of the potentially allocated memory
+               os9       F$AllBit            allocate the bits in the memory map
+               ldd       _stkStrtPg@,s       get the starting page of the now-allocated memory in A and the requested additional amount in B
+               suba      _stkPageCnt@,s      subtract the page count on the stack from the starting page of the now-allocated memory
+               addb      _stkPageCnt@,s      add the page count to the requested additional amount... this is the total amount
+               puls      u,y,x               restore the registers
                ldx       <D.Proc             get the current process descriptor
-               bra       L0225
-L0207          negb      
-               tfr       d,y
-               negb      
-               addb      P$PagCnt,x          add the page count
-               addb      P$ADDR,x            and the base data address page
+               bra       L0225               jump around shrink code
+* If we get here, the caller is asking for LESS memory, so shrinking the process' amount.
+memshrink@     negb                          negate the number of pages
+               tfr       d,y                 put D in Y
+               negb                          restore the negated number of pages
+               addb      P$PagCnt,x          add the page count to B
+               addb      P$ADDR,x            and the base data address page to B
                cmpb      P$SP,x              compare it to the caller's stack pointer
                bhi       L0217               branch if we're higher
                comb                          else set the carry flag
@@ -76,10 +79,10 @@ L0207          negb
                rts                           return to the caller
 L0217          ldx       <D.FMBM             get the free memory bitmap pointer
                os9       F$DelBit            delete the bits
-               tfr       y,d
-               negb      
+               tfr       y,d                 transfer # of bits to clear into D
+               negb                          negate B
                ldx       <D.Proc             get the current process descriptor
-               addb      P$PagCnt,x
+               addb      P$PagCnt,x          add the page count
                lda       P$ADDR,x            get the process' base data address page
 L0225          std       P$ADDR,x            store the process' base data address page and page count
 returnsize@    lda       P$PagCnt,x          get the page count
@@ -92,6 +95,8 @@ ex@            comb                          set the carry flag
                ldb       #E$MemFul           indicate memory is full
                puls      pc,u,y,x            restore registers and return to the caller
 
+* Round up to the next page, then swap A and B
+* Example: A=$10, B=$33; result is A=$00, B=$11
 RoundUpD       addd      #$00FF              add 255 to D
                clrb                          clear B
                exg       a,b                 swap the registers
