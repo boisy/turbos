5db2ea31f69bda0a43d60ae2513297dae8b230ce
Boisy Pitre <bpitre@apple.com>
2025-02-03 07:43:29 -0600

    Clean up location of include files

--- ports/turbo9sim/defs.d
+++ ports/turbo9sim/defs.d
@@ -3,10 +3,3 @@
 
 * F256 Jr. specific definitions
                use       turbo9sim.d
-
-* F256 Jr. mapped I/O boundaries
-MappedIOStart  set       $FF00
-MappedIOEnd    set       $FFFF
-
-* F256 Jr. ticks per second support
-TkPerSec       set       60
